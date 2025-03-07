46d8bb96bc4ad2f29d996c19ce68a84cbbce800e
jakoblundoe <dev@jakoblundoe.com>
2025-03-07 13:04:54 +0100

    Major refactor + check if selected tracks contain folder tracks

--- JL_Create Resample Track.lua
+++ JL_Create Resample Track.lua
@@ -6,7 +6,7 @@
 -- USER CONFIG AREA -----------------------------------------------------------
 
 -- Set Track Height A in pixels(default).
-TrackHeight_A = 116
+MinimizedHeight = 116
 
 -- Set same track color. If several tracks selected the first selected track color is copied.
 SetSameTrColor = true
@@ -15,72 +15,97 @@ SetSameTrColor = true
 
 local defsendvol = ({reaper.BR_Win32_GetPrivateProfileString('REAPER', 'defsendvol', '1',  reaper.get_ini_file() )})[2]
 local defsendflag = ({reaper.BR_Win32_GetPrivateProfileString('REAPER', 'defsendflag', '0',  reaper.get_ini_file() )})[2]
+local isFolderTrackSelected = false
+local selectedTracksArr = {}
+local maxTrackNumber = -math.huge
+local minTrackNumber = math.huge
 
+reaper.ClearConsole()
 function Print(param)
-  reaper.ClearConsole()
   reaper.ShowConsoleMsg(tostring(param).."\n")
 end
 
 function Main()
   TrackSum = reaper.CountSelectedTracks(0)
-  local maxTrackNumber = -math.huge
-  local minTrackNumber = math.huge
 
-    -- get first and last (of selected tracks) position
-    for i = 1, TrackSum do
-      local tr = reaper.GetSelectedTrack(0, i - 1)
-      local trPosNumber = reaper.GetMediaTrackInfo_Value(tr, "IP_TRACKNUMBER")
-      minTrackNumber = math.min(minTrackNumber, trPosNumber)
-      maxTrackNumber = math.max(maxTrackNumber, trPosNumber)
-    end
-  
-    FirstSelTr = reaper.GetTrack(0, minTrackNumber - 1)
-    local retval, FirstSelTrName = reaper.GetTrackName(FirstSelTr)
-  
-  -- popup window to input new destination track name. Is populated with ReSmpl + first selected track name
-  local returnValue, sname = reaper.GetUserInputs('Send name', 1, 'Resampling track name' ,'ReSmpl: ' .. FirstSelTrName)
-
-  if returnValue then 
-
-    NewDestTr = InsertTrackBelowSelTracks(maxTrackNumber)
-
-    for i = 1, TrackSum do
-      local trackID = reaper.GetSelectedTrack(0, i - 1)
-      NewSendID = reaper.CreateTrackSend(trackID, NewDestTr)
-      reaper.SetTrackSendInfo_Value(trackID, 0, NewSendID, 'D_VOL', defsendvol)
-      reaper.SetTrackSendInfo_Value(trackID, 0, NewSendID, 'I_SENDMODE', defsendflag)
-      SetOriginTrackRecState(trackID)
-    end
-
-    if NewSendID >= 0 then
-      if NewDestTr then
-        SetDestTrackColor(FirstSelTr, NewDestTr)
-
-        -- select created track
-        reaper.SetOnlyTrackSelected(NewDestTr)
-
-        reaper.GetSetMediaTrackInfo_String(NewDestTr, 'P_NAME', sname ,true)
-        reaper.SetMediaTrackInfo_Value(NewDestTr, "I_RECMODE_FLAGS", 2)
-        reaper.SetMediaTrackInfo_Value(NewDestTr, "I_RECMODE", 1)
-        reaper.SetMediaTrackInfo_Value(NewDestTr, "I_RECARM", 1)
-        reaper.SetMediaTrackInfo_Value(NewDestTr, "B_MUTE", 1)
-
-        -- set track height to custom track_height
-        reaper.SetMediaTrackInfo_Value(NewDestTr, "I_HEIGHTOVERRIDE", TrackHeight_A)
-
-        --  set to "Display gain reduction in track meters for plug-ins that support it
-        reaper.Main_OnCommand(42705, 0)
+  for i = 1, TrackSum do
+    local tr = reaper.GetSelectedTrack(0, i - 1)
+    -- insert selected tracks into array
+    table.insert(selectedTracksArr, tr)
+    --- check for folder tracks and set global bool
+    if CheckFolderTracksSelected(tr) == 1 then
+      if not isFolderTrackSelected then
+        isFolderTrackSelected = true
       end
     end
-    reaper.TrackList_AdjustWindows(false)
+    -- get first and last (of selected tracks) position
+    local trPosNumber = reaper.GetMediaTrackInfo_Value(tr, "IP_TRACKNUMBER")
+    minTrackNumber = math.min(minTrackNumber, trPosNumber)
+    maxTrackNumber = math.max(maxTrackNumber, trPosNumber)
+  end
+
+  local returnvalue, trackname = setNewTrackName(minTrackNumber)
+  -- if a folder track is selected
+    -- put code here
+
+
+
+
+  -- if no folder track is selected
+  -- insert track (if script name isn't cancelled)
+  if returnvalue then 
+    local newDestTr = InsertTrackBelowSelTracks(maxTrackNumber)
+    ConfigureNewTrack(newDestTr, trackname)
+  end
+end
+
+function setNewTrackName(minTrackNumber)
+  FirstSelTr = reaper.GetTrack(0, minTrackNumber - 1)
+  local retval, FirstSelTrName = reaper.GetTrackName(FirstSelTr)
+  if retval then
+    -- popup window to input new destination track name. Is populated with ReSmpl + first selected track name
+    returnvalue, strname = reaper.GetUserInputs('Send name', 1, 'Resampling track name' ,'ReSmpl: ' .. FirstSelTrName)
+    return returnvalue, strname
   end
 end
 
 -- insert track right below last selected track
 function InsertTrackBelowSelTracks(lastSelTrackPos)
   reaper.InsertTrackAtIndex(lastSelTrackPos, true)
-  local NewDestTr = reaper.GetTrack(0, lastSelTrackPos)
-  return NewDestTr
+  local newDestTr = reaper.GetTrack(0, lastSelTrackPos)
+  return newDestTr
+end
+
+function ConfigureNewTrack(tr, trackname)
+  for i = 1, TrackSum do
+    local trackID = reaper.GetSelectedTrack(0, i - 1)
+    NewSendID = reaper.CreateTrackSend(trackID, tr)
+    reaper.SetTrackSendInfo_Value(trackID, 0, NewSendID, 'D_VOL', defsendvol)
+    reaper.SetTrackSendInfo_Value(trackID, 0, NewSendID, 'I_SENDMODE', defsendflag)
+    SetOriginTrackRecState(trackID)
+  end
+
+  if NewSendID >= 0 then
+    if tr then
+      SetDestTrackColor(FirstSelTr, tr)
+
+      -- select created track
+      reaper.SetOnlyTrackSelected(tr)
+
+      reaper.GetSetMediaTrackInfo_String(tr, 'P_NAME', trackname ,true)
+      reaper.SetMediaTrackInfo_Value(tr, "I_RECMODE_FLAGS", 2)
+      reaper.SetMediaTrackInfo_Value(tr, "I_RECMODE", 1)
+      reaper.SetMediaTrackInfo_Value(tr, "I_RECARM", 1)
+      reaper.SetMediaTrackInfo_Value(tr, "B_MUTE", 1)
+
+      -- set track height to custom track_height
+      reaper.SetMediaTrackInfo_Value(tr, "I_HEIGHTOVERRIDE", MinimizedHeight)
+
+      --  set to "Display gain reduction in track meters for plug-ins that support it
+      reaper.Main_OnCommand(42705, 0)
+    end
+  end
+  reaper.TrackList_AdjustWindows(false)
 end
 
 -- set track color of new destination track
@@ -117,6 +142,11 @@ function SetOriginTrackRecState(originTrack)
     reaper.SetMediaTrackInfo_Value(originTrack, "I_RECARM", 0)
     reaper.SetMediaTrackInfo_Value(originTrack, "I_RECMODE", 0)
   end
+end
+
+function CheckFolderTracksSelected (tr)
+  local checkFolder = reaper.GetMediaTrackInfo_Value(tr, "I_FOLDERDEPTH")
+  return checkFolder
 end
 
 Main()
