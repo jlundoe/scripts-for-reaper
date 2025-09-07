79b1a86486768ba1a60cfed4fea79da07c231cb4
jakoblundoe <dev@jakoblundoe.com>
2025-09-07 20:33:59 +0200

    Edit comments

--- JL_Adjust Item Volume (relative).lua
+++ JL_Adjust Item Volume (relative).lua
@@ -34,7 +34,7 @@ function main()
             local sourceType = reaper.GetMediaSourceType(itemSource)
             -- check if audio item
             if sourceType ~= "MIDI" then
-                -- check midi cc and adjust volume accordingly
+                -- check midi cc (up or down indication) and adjust volume accordingly
                 if MidiCCplaceholder == 63 then
                     -- calculate new volume
                     local newItemVol = setNewVolValue(itemVol, DBincr, false)
@@ -54,35 +54,5 @@ function main()
         end
     end
 end
-
-
-
--- if audio item
-    -- check midi cc value
-    -- if 65 turn up by "var-amount"
-    -- if 63 turn down by "var-amount"
-
-
-
---     -- set audio items rnd volume change
---     -- loop through items in table
---     for key, value in pairs(selAudioItems) do
---         local currentItemVol
---         if relVolumeVariation then
---             -- get current item volume
---             currentItemVol = reaper.GetMediaItemInfo_Value(value, "D_VOL")
---         else
---             -- reset item volume to zero
---             currentItemVol = 1
---         end
---         -- generate new item volume (new value relative to it's previous volume)
---         local newItemVol = generateNewVolValue(currentItemVol, maxVolumeVariation)
---         -- set item to new random volume
---         reaper.SetMediaItemInfo_Value(value, "D_VOL", newItemVol)
-    
---         -- set items to generated colorcode (as indicator)
---         reaper.SetMediaItemInfo_Value(value, "I_CUSTOMCOLOR", colorCode|0x1000000)
---     end
--- end
 
 main()
