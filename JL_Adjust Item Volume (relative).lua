c4ec24ecd0db2abebbef33449fa39df76a086db5
jakoblundoe <dev@jakoblundoe.com>
2025-09-07 21:17:48 +0200

    Prevent error msg when no item is hovered

--- JL_Adjust Item Volume (relative).lua
+++ JL_Adjust Item Volume (relative).lua
@@ -28,33 +28,34 @@ function main()
     reaper.BR_GetMouseCursorContext()
     -- get current item which cursor is hovering over
     local item = reaper.BR_GetMouseCursorContext_Item()
-    local itemVol = reaper.GetMediaItemInfo_Value(item, "D_VOL")
-    -- reaper.ShowConsoleMsg(tostring(itemVol))
-    -- check item type
-    if item ~= nil then
-        local activeTake = reaper.GetActiveTake(item)
-        if activeTake ~= nil then
-            local itemSource = reaper.GetMediaItemTake_Source(activeTake)
-            local sourceType = reaper.GetMediaSourceType(itemSource)
-            -- check if audio item
-            if sourceType ~= "MIDI" then
-                -- check midi cc (up or down indication) and adjust volume accordingly
-                local is_new_value,filename,sectionID,cmdID,mode,resolution,val,contextstr = reaper.get_action_context()
-                if val <= 63 then
-                    -- calculate new volume
-                    local newItemVol = setNewVolValue(itemVol, dbIncrement, false)
-                    -- set new volume
-                    reaper.SetMediaItemInfo_Value(item, "D_VOL", newItemVol)
-                elseif val >= 65 then
-                    -- calculate new volume
-                    local newItemVol = setNewVolValue(itemVol, dbIncrement, true)
-                    -- set new volume
-                    reaper.SetMediaItemInfo_Value(item, "D_VOL", newItemVol)
+    if item then
+        local itemVol = reaper.GetMediaItemInfo_Value(item, "D_VOL")
+        -- check item type
+        if item ~= nil then
+            local activeTake = reaper.GetActiveTake(item)
+            if activeTake ~= nil then
+                local itemSource = reaper.GetMediaItemTake_Source(activeTake)
+                local sourceType = reaper.GetMediaSourceType(itemSource)
+                -- check if audio item
+                if sourceType ~= "MIDI" then
+                    -- check midi cc (up or down indication) and adjust volume accordingly
+                    local is_new_value,filename,sectionID,cmdID,mode,resolution,val,contextstr = reaper.get_action_context()
+                    if val <= 63 then
+                        -- calculate new volume
+                        local newItemVol = setNewVolValue(itemVol, dbIncrement, false)
+                        -- set new volume
+                        reaper.SetMediaItemInfo_Value(item, "D_VOL", newItemVol)
+                    elseif val >= 65 then
+                        -- calculate new volume
+                        local newItemVol = setNewVolValue(itemVol, dbIncrement, true)
+                        -- set new volume
+                        reaper.SetMediaItemInfo_Value(item, "D_VOL", newItemVol)
+                    else
+                        reaper.ShowConsoleMsg("do nothing")
+                    end
                 else
-                    reaper.ShowConsoleMsg("do nothing")
+                    -- reaper.ShowConsoleMsg("ignoring midi item")
                 end
-            else
-                -- reaper.ShowConsoleMsg("ignoring midi item")
             end
         end
     end
